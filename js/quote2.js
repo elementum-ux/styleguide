500473a529b91341cb2c8f7317909f888198f766
tony <tony@elementum.com>
2014-05-07 16:07:54 -0700

    -

--- js/quote2.js
+++ js/quote2.js
@@ -1,4 +1,4 @@
-var mess = new Array('Think outside the &#60;div&#62;<br/><small>KRISTOF ORTS</small>','Users will tell you what they think they want.<br/>Users will tell you what they think you want to hear.<br/>Users will tell you what they think sounds good.<br/>Users will not tell you what you need to know.<br/>You have to watch them to discover that.<br/><small>ADAM JUDGE</small>','No matter how cool your interface is,<br/>it would be better if there were less of it.<br/><small>ALAN COOPER</small>','A beef filet cooked for 15 hours by 30 cooks doesn’t necessarily taste better than a cheeseburger.<br/><small>OLIVER REICHENSTEIN</small>');
+var mess = new Array('Think outside the &#60;div&#62;<br/><small>KRISTOF ORTS</small>','Design is intelligence made visible.<br/><small>ALINA WHEELER</small>','No matter how cool your interface is,<br/>it would be better if there were less of it.<br/><small>ALAN COOPER</small>','A beef filet cooked for 15 hours by 30 cooks doesn’t necessarily taste better than a cheeseburger.<br/><small>OLIVER REICHENSTEIN</small>');
 var max = mess.length;
 var num = Math.floor((Math.random() * max));
 document.writeln(mess[num]);
